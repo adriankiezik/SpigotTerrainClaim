3dee1626461d36492b65464acf680db4261414b2
adriankiezik <kiezikadrian@gmail.com>
2022-03-18 17:44:26 +0100

    Init commit

---
+++ src/main/java/me/yumei/terrainclaim/TerrainClaim.java
@@ -0,0 +1,11 @@
+package me.yumei.terrainclaim;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TerrainClaim extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
